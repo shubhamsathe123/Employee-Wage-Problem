03686417f2ad12efe0525de0da166ecd86cfd545
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-22 15:31:05 +0530

    checked half day employee wage

--- utility.js
+++ utility.js
@@ -2,6 +2,7 @@ class Utility{
     
    attendance_check()
    {
+    const Half_Day_hr=4;
     const Full_day_hr=8;
     const wage_per_hr=20;
     var empCheck = Math.floor(Math.random() * 2);
@@ -14,8 +15,10 @@ class Utility{
     {
         console.log("employee is absent");
     }
-    var daily_emp_wage=Full_day_hr * wage_per_hr * empCheck;
-    console.log("full day employee wage is "+daily_emp_wage);
+    var full_day_wage=Full_day_hr * wage_per_hr * empCheck;
+    console.log("full day employee wage is "+full_day_wage);
+    var half_day_wage=Half_Day_hr * wage_per_hr * empCheck;
+    console.log("part time employee wage for the day is "+half_day_wage);
    }
 }
 
