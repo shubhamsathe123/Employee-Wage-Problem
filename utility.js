c40cd1d0cafac80897b2250070bcbf237251cab7
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-23 12:45:30 +0530

    refactor:updated final code

--- utility.js
+++ utility.js
@@ -5,8 +5,7 @@ class Utility{
     let Half_day_hr=4;
     let Full_day_hr=8;
     var availability;
-     var empCheck = Math.floor(Math.random() * 10 % 3);
-     
+    var empCheck = Math.floor(Math.random() * 3);
      switch(empCheck)
      {
        case 0:
@@ -24,11 +23,9 @@ class Utility{
                 availability=Full_day_hr;
                 break;
             }
-
      }
     return availability;
    }
-
    wage_calculation()
    {  
       const MAX_MONTHLY_HRS = 100;
@@ -37,13 +34,15 @@ class Utility{
        var day = 0;
        var totalHrs = 0;
        var monthly_Wage=0;
+       var check=this.attendance_check();
         for(var day =0;day <= TOTAL_MONTHLY_DAYS;day ++)
         { 
-         if(totalHrs <= MAX_MONTHLY_HRS ){
-            var daily_wage=wage_per_hour * this.attendance_check();  
+         if(totalHrs <= MAX_MONTHLY_HRS )
+         {
+            var daily_wage=wage_per_hour * check;  
             monthly_Wage = monthly_Wage + daily_wage;
             console.log("Day:"+day+" | Daily Wage : "+daily_wage+ " | Total Wage : "+monthly_Wage);
-            totalHrs = totalHrs + this.attendance_check();
+            totalHrs = totalHrs + check;
          }
         }
         console.log("monthly wage is"+monthly_Wage);
